e7a61a01cf29aa012b1df442b9e50ef4a67fa5af
melizahaiziK <melizaha1zik@gmail.com>
2026-05-06 11:15:53 +0700

    praktikum 1

---
+++ Pertemuan12/NodeMahasiswa11.java
@@ -0,0 +1,10 @@
+package Pertemuan12;
+public class NodeMahasiswa11 {
+    Mahasiswa11 data;
+    NodeMahasiswa11 next;
+
+    public NodeMahasiswa11(Mahasiswa11 data, NodeMahasiswa11 next) {
+        this.data = data;
+        this.next = next;
+    }
+}
